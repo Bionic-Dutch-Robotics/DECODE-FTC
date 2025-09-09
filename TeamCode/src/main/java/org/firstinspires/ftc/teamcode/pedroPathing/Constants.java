7b4942540a9e62bc03b8e61ac81414f0ac2941c0
DC-Al-Coder <atharvgurnani@gmail.com>
2025-09-09 01:13:43 +0000

    Add Pinpoint Localizer and Mecanum Constants with Placeholder Values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -7,13 +7,39 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+        .mass(13);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+        .maxPower(1)
+        .leftFrontMotorName("frontLeft")
+        .leftRearMotorName("backLeft")
+        .rightFrontMotorName("frontRight")
+        .rightRearMotorName("backRight")
+        .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+        .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+        .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+        .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+                .forwardPodY(-5)
+                .distanceUnit(DistanceUnit.INCH)
+                .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+                .forwardEncoderDirection(Encoder.REVERSE)
+                .strafeEncoderDirection(Encoder.FORWARD)
+                .strafePodX(0.5);
+
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
+                .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+                .strafeEncoderDirection(Encoder.REVERSE)
                 .build();
-    }
 }
+
+
